514b1f90e3fcb41f28eda8eeb95858da5e882978
ramesh <rameshricky24@gmail.com>
2024-04-02 23:22:02 +0530

    disabling chatBox until user selects user to chat with

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Messages from './Messages';
 import ChatInput from './ChatInput';
 import InfoHeader from './InfoHeader';
+import { ChatContext } from '../context/chatContext';
 
 const ChatBox = () => {
+
+  const {data} = useContext(ChatContext)
+  console.log(data)
+
+  if(data.chatId==="null")
+    return <div className='chatBox'></div>
   return (
     <div className="chatBox">
       
